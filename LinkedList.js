2a95533d396a67d7904cad587b24d93f37682416
Joseph Fischetti <fischettijw@gmail.com>
2021-02-16 07:59:15 -0500

    initial

---
+++ LinkedList.js
@@ -0,0 +1,34 @@
+class LinkedList {
+    constructor() {
+        this.length = 0;
+        this.head = null;
+        this.tail = null;
+    }
+
+    prepend(data) {
+        const node = new Node(data);
+        if (!this.head) {
+            this.head = node;
+            this.tail = node;
+            this.length++;
+        } else {
+            node.next = this.head;
+            this.head = node;
+            this.length++;
+        }
+    }
+
+    display() {
+        const output = [];
+        console.log(`Length: ${this.length} || Head: ${this.head.data} || Tail: ${this.tail.data}`);
+        let counter = 0;
+        let current = this.head
+        while (counter < this.length) {
+            output.push(current.data);
+            current = current.next;
+            counter++;
+        }
+        console.log(output);
+    }
+
+}
---
+++ Main.js
@@ -0,0 +1,15 @@
+console.clear();
+
+// const node = new Node(1);
+// console.log(node);
+//--------------------------------------------------------
+
+const linkedList = new LinkedList();
+linkedList.prepend("1st Node");
+// console.log(linkedList);
+
+linkedList.prepend("2nd Node");
+linkedList.prepend("3rd Node");
+linkedList.prepend("4th Node");
+// console.log(linkedList);
+linkedList.display();
