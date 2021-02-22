8b95976db1df5cd35e535f3f117397da2ed4f94a
Joseph Fischetti <fischettijw@gmail.com>
2021-02-22 13:14:44 -0500

    added reverse

--- Main.js
+++ Main.js
@@ -37,10 +37,22 @@ const linkedList = new LinkedList();
 for (let i = 0; i < 8; i++) {
     linkedList.append(`${i} appended`);
 }
+// linkedList.display();
+// linkedList.delete(0);
+// linkedList.display();
+// linkedList.delete(linkedList.length - 1);
+// linkedList.display();
+// linkedList.delete(3);
+// linkedList.display();
+
+
+// linkedList.append(1);
+// linkedList.append(2);
+// linkedList.append(3);
+// console.log(linkedList);
+
+
 linkedList.display();
-linkedList.delete(0);
-linkedList.display();
-linkedList.delete(linkedList.length - 1);
-linkedList.display();
-linkedList.delete(3);
+
+linkedList.reverse();
 linkedList.display();
